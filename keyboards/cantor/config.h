3fbdc5848849366039f4f4a25af947463be0513c
Lan Zongwei <lanzongwei541@gmail.com>
2025-05-02 14:53:27 +0800

    feat: add cantor lzw layout

--- keyboards/cantor/config.h
+++ keyboards/cantor/config.h
@@ -6,6 +6,7 @@
 #define SERIAL_USART_FULL_DUPLEX   // Enable full duplex operation mode.
 #define SERIAL_USART_TX_PIN B6     // USART TX pin
 #define SERIAL_USART_RX_PIN B7     // USART RX pin
+#define DYNAMIC_MACRO_NO_NESTING
 
 /*
  * Feature disable options
